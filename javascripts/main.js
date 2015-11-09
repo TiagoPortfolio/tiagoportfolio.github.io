6dd992a9bae17368a616aca3f00c912f352dbc65
Tiago Sousa <tiagosousa028@gmail.com>
2015-11-09 02:01:41 +0000

    remove console.log()

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,10 +11,11 @@ $(document).ready(function(){
 		e.preventDefault();
 		if($(this).hasClass('focused'))
 			return false;
+		
 		$("#header_wrap a.focused").toggleClass("focused");
 		$(this).toggleClass('focused');
 		var randomAnimation = animations[Math.floor(Math.random()*animations.length)];
-		console.log(randomAnimation);
+
 		$(this).addClass('animated ' + randomAnimation)
 			.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 				$(this).removeClass('animated ' + randomAnimation);
