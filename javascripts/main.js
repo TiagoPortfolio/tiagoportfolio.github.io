8c441940ff9284e97c3d12528594846783565f3c
TiagoPortfolio <tiagosousa028@gmail.com>
2017-12-28 18:54:40 +0000

    Add fadeIn/Out to change pages and fix flickity

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,85 +1,7 @@
 $(document).ready(function(){
 
-	// HEADER ANIMATIONS
-	var animations = [
-	'flash', 'rubberBand',
-	'swing', 'wobble',
-	'jello', 'flip'
-	];
+	var activePage = 1;
 
-	$('.anim').click(function(e){
-		e.preventDefault();
-		if($(this).hasClass('focused'))
-			return false;
-		
-		var randomAnimation = animations[Math.floor(Math.random()*animations.length)];
-		$(this).addClass('animated ' + randomAnimation)
-		.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-			$(this).removeClass('animated ' + randomAnimation);
-		});
-	});
-
-	var $projectsCarousel = $('.gallery').flickity({
-	  // options, defaults listed
-	  accessibility: true,
-	  // enable keyboard navigation, pressing left & right keys
-	  autoPlay: false,
-	  // advances to the next cell
-	  // if true, default is 3 seconds
-	  // or set time between advances in milliseconds
-	  // i.e. `autoPlay: 1000` will advance every 1 second
-	  cellAlign: 'center',
-	  // alignment of cells, 'center', 'left', or 'right'
-	  // or a decimal 0-1, 0 is beginning (left) of container, 1 is end (right)
-	  cellSelector: undefined,
-	  // specify selector for cell elements
-	  contain: false,
-	  // will contain cells to container
-	  // so no excess scroll at beginning or end
-	  // has no effect if wrapAround is enabled
-	  draggable: true,
-	  // enables dragging & flicking
-	  freeScroll: false,
-	  // enables content to be freely scrolled and flicked
-	  // without aligning cells
-	  friction: 0.2,
-	  // smaller number = easier to flick farther
-	  initialIndex: 1,
-	  // zero-based index of the initial selected cell
-	  lazyLoad: true,
-	  // enable lazy-loading images
-	  // set img data-flickity-lazyload="src.jpg"
-	  // set to number to load images adjacent cells
-	  percentPosition: true,
-	  // sets positioning in percent values, rather than pixels
-	  // Enable if items have percent widths
-	  // Disable if items have pixel widths, like images
-	  prevNextButtons: true,
-	  // creates and enables buttons to click to previous & next cells
-	  pageDots: true,
-	  // create and enable page dots
-	  resize: true,
-	  // listens to window resize events to adjust size & positions
-	  rightToLeft: false,
-	  // enables right-to-left layout
-	  setGallerySize: true,
-	  // sets the height of gallery
-	  // disable if gallery already has height set with CSS
-	  watchCSS: false,
-	  // watches the content of :after of the element
-	  // activates if #element:after { content: 'flickity' }
-	  // IE8 and Android 2.3 do not support watching :after
-	  // set watch: 'fallbackOn' to enable for these browsers
-	  wrapAround: false
-	  // at end of cells, wraps-around to first for infinite scrolling
-	});
-
-	//PROJECT SLIDER
-	$projectsCarousel.on('cellSelect staticClick', function(event, pointer, cellElement, cellIndex) {
-		if(event.type == "staticClick") {
-			$projectsCarousel.flickity('select', cellIndex);
-		}
-	});
 	$ptButton = $('.pt-button');
 
 	$ptButton.on('click', function() {
@@ -91,10 +13,51 @@ $(document).ready(function(){
 			$(this).toggleClass('focused');
 		}
 
+		$("#header_wrap div:nth-child(2) a:not(.focused)").each(function(){
+			$(this).fadeOut(150, function() {
+				$(this).show().css({visibility: "hidden"});
+	    	});
+		});
 
+		setTimeout(function() {
+			$("#header_wrap div:nth-child(2) a:not(.focused)").each(function(){
+				$(this).hide().css({visibility: "visible"}).fadeIn(150);
+	    	});
+		}, 300);
+
+		let page = $(this).attr("data-page");
+		if (activePage != page) {
+			$(".pt-page-" + activePage).fadeOut(function() {
+				$(".pt-page-" + page).fadeIn();
+				// If projects page, initialize slider before fadeIn()
+				if (page == 2) {
+					$('.gallery').flickity({
+					  accessibility: true,
+					  autoPlay: false,
+					  cellAlign: 'center',
+					  cellSelector: undefined,
+					  contain: false,
+					  draggable: true,
+					  freeScroll: false,
+					  friction: 0.2,
+					  initialIndex: 1,
+					  lazyLoad: true,
+					  percentPosition: true,
+					  prevNextButtons: true,
+					  pageDots: true,
+					  resize: true,
+					  rightToLeft: false,
+					  setGallerySize: true,
+					  watchCSS: false,
+					  wrapAround: false
+					});
+				}
+			});
+			activePage = page;
+		}
 
 	    //page = 2
-	    if(2){
+	    if(0){
 	    	$("#skills-page .container > div:nth-child(1)").addClass('flash_div')
 					.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 						$(this).removeClass('flash_div');
