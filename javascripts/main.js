40aa393b7d82d76c84ef15b90ed5a5735e0efe1b
TiagoPortfolio <tiagosousa028@gmail.com>
2018-01-04 16:45:49 +0000

    Remove CSS rules not being used

--- javascripts/main.js
+++ javascripts/main.js
@@ -47,7 +47,7 @@ $(document).ready(function(){
 					  pageDots: true,
 					  resize: true,
 					  rightToLeft: false,
-					  setGallerySize: true,
+					  setGallerySize: false,
 					  watchCSS: false,
 					  wrapAround: false
 					});
