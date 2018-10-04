3ff7b8f02a2ffa62d7af56fb265bd7a990bcf1e5
TiagoPortfolio <tiagosousa028@gmail.com>
2018-10-04 18:08:22 +0100

    Update drum machine with creat-react-app 2.0.3

---
+++ projects/react-apps/drum-machine/build/precache-manifest.4156dd8f937a7537d03bd60ea2713e6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projects/react-apps/drum-machine/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f0e0acd2fd55162b4c51",
+    "url": "/projects/react-apps/drum-machine/static/js/runtime~main.f0e0acd2.js"
+  },
+  {
+    "revision": "e31356e31e2162c66a19",
+    "url": "/projects/react-apps/drum-machine/static/js/main.e31356e3.chunk.js"
+  },
+  {
+    "revision": "0ee12f708735bcbf0b9f",
+    "url": "/projects/react-apps/drum-machine/static/js/1.0ee12f70.chunk.js"
+  },
+  {
+    "revision": "e31356e31e2162c66a19",
+    "url": "/projects/react-apps/drum-machine/static/css/main.fc4c54f3.chunk.css"
+  },
+  {
+    "revision": "7673a9777cae1f98f272640996e8d04f",
+    "url": "/projects/react-apps/drum-machine/index.html"
+  }
+];
