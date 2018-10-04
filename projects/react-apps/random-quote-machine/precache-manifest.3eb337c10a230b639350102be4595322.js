40886f8ee5cb8cd3177b13b8917df4463d5050f7
TiagoPortfolio <tiagosousa028@gmail.com>
2018-10-04 18:27:19 +0100

    Update random quote machine react app + update drum machine build folder

---
+++ projects/react-apps/random-quote-machine/precache-manifest.3eb337c10a230b639350102be4595322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0623385d6e275b40ca09",
+    "url": "/static/js/main.0623385d.chunk.js"
+  },
+  {
+    "revision": "efb754673584cfda9b28",
+    "url": "/static/js/1.efb75467.chunk.js"
+  },
+  {
+    "revision": "0623385d6e275b40ca09",
+    "url": "/static/css/main.31df6c90.chunk.css"
+  },
+  {
+    "revision": "13e2eb13dd665dca69f11e7106e139ec",
+    "url": "/index.html"
+  }
+];
