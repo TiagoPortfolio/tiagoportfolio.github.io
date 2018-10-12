eb2b9dd5d6144ca383085cc52636c57ed2cbf0d6
TiagoPortfolio <tiagosousa28@hotmail.com>
2018-10-12 17:37:29 +0100

    update quote-machine react app

--- projects/react-apps/quote-machine/service-worker.js
+++ projects/react-apps/quote-machine/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/projects/react-apps/quote-machine/precache-manifest.ec8ef0d90fd35019b392c12b3712bb7f.js"
+  "/projects/react-apps/quote-machine/precache-manifest.a6b7a847f57b6aa6a0ebf84127d72a6f.js"
 );
 
 workbox.clientsClaim();
