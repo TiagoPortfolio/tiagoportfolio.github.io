b04b5fec24a0a1b2eb20dd7d73ba89d4fc8ca942
TiagoPortfolio <tiagosousa28@hotmail.com>
2018-12-18 23:21:13 +0000

    Add tesla battery react project

---
+++ projects/react-apps/tesla-battery/build/precache-manifest.187c950aa6476f37610d335f4333c18f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "37862a62ee2074b44d66b00fb9c1ebce",
+    "url": "/projects/react-apps/tesla-battery/static/media/75.37862a62.svg"
+  },
+  {
+    "revision": "d30c52be0f6cdf834002",
+    "url": "/projects/react-apps/tesla-battery/static/css/main.f6b50d88.chunk.css"
+  },
+  {
+    "revision": "d3ec7fa12725f7c45993",
+    "url": "/projects/react-apps/tesla-battery/static/js/1.d3ec7fa1.chunk.js"
+  },
+  {
+    "revision": "5de1776df32b926a627f",
+    "url": "/projects/react-apps/tesla-battery/static/js/runtime~main.5de1776d.js"
+  },
+  {
+    "revision": "99c8f2040d559a70d47006716713add5",
+    "url": "/projects/react-apps/tesla-battery/static/media/logo.99c8f204.svg"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/projects/react-apps/tesla-battery/static/media/Roboto-Regular-webfont.30799efa.eot"
+  },
+  {
+    "revision": "7f1320f7ec4f6716054d88c33235d17b",
+    "url": "/projects/react-apps/tesla-battery/static/media/Roboto-Regular-webfont.7f1320f7.ttf"
+  },
+  {
+    "revision": "3e5675c89f974f7811eeaf07e2dd5ba3",
+    "url": "/projects/react-apps/tesla-battery/static/media/Roboto-Regular-webfont.3e5675c8.woff"
+  },
+  {
+    "revision": "da61d7ef302b8bf871dd0ae796a52f33",
+    "url": "/projects/react-apps/tesla-battery/static/media/Roboto-Regular-webfont.da61d7ef.svg"
+  },
+  {
+    "revision": "ff16203f3d0aa6c8a55315d4e3e7b287",
+    "url": "/projects/react-apps/tesla-battery/static/media/tesla.ff16203f.jpg"
+  },
+  {
+    "revision": "e787dc928af0297e0c30ebf60ca1e0df",
+    "url": "/projects/react-apps/tesla-battery/static/media/wheel-21.e787dc92.png"
+  },
+  {
+    "revision": "3b975a003195f4fbb447ee79965fee91",
+    "url": "/projects/react-apps/tesla-battery/static/media/wheel-19.3b975a00.png"
+  },
+  {
+    "revision": "7ee66eea8c5fcc82180de8776e3e37ce",
+    "url": "/projects/react-apps/tesla-battery/static/media/60.7ee66eea.svg"
+  },
+  {
+    "revision": "d30c52be0f6cdf834002",
+    "url": "/projects/react-apps/tesla-battery/static/js/main.d30c52be.chunk.js"
+  },
+  {
+    "revision": "246b5ba5f8b8af340199831b3a4b538b",
+    "url": "/projects/react-apps/tesla-battery/static/media/60d.246b5ba5.svg"
+  },
+  {
+    "revision": "28a215f09ce0146c0b0e22fe2664c536",
+    "url": "/projects/react-apps/tesla-battery/static/media/90d.28a215f0.svg"
+  },
+  {
+    "revision": "9c937cb94afc2cdccc2cc4cf06dcbfe6",
+    "url": "/projects/react-apps/tesla-battery/static/media/75d.9c937cb9.svg"
+  },
+  {
+    "revision": "62ed87b41017976b4ffff063c91288cf",
+    "url": "/projects/react-apps/tesla-battery/static/media/p100d.62ed87b4.svg"
+  },
+  {
+    "revision": "b3fc73bf1e58ec43c8502bbbc089913c",
+    "url": "/projects/react-apps/tesla-battery/static/media/up.b3fc73bf.svg"
+  },
+  {
+    "revision": "a56566ba32341898ea4772990a826e86",
+    "url": "/projects/react-apps/tesla-battery/static/media/down.a56566ba.svg"
+  },
+  {
+    "revision": "987170eab046c4310d198d864f2dc8d7",
+    "url": "/projects/react-apps/tesla-battery/static/media/heat-off.987170ea.svg"
+  },
+  {
+    "revision": "4dcccadfb884623189abd5d22d7777fc",
+    "url": "/projects/react-apps/tesla-battery/static/media/ac-on.4dcccadf.svg"
+  },
+  {
+    "revision": "c418a045056d38fac1b7c87898e1506b",
+    "url": "/projects/react-apps/tesla-battery/static/media/ac-off.c418a045.svg"
+  },
+  {
+    "revision": "2c61b1a6023103f04fb44e813e4a9a7b",
+    "url": "/projects/react-apps/tesla-battery/static/media/heat-on.2c61b1a6.svg"
+  },
+  {
+    "revision": "a52693ae2ceb4b6315fecaf078a0bdbe",
+    "url": "/projects/react-apps/tesla-battery/static/media/21.a52693ae.svg"
+  },
+  {
+    "revision": "c8ef339847339a7f374dc08857c0413b",
+    "url": "/projects/react-apps/tesla-battery/static/media/19.c8ef3398.svg"
+  },
+  {
+    "revision": "34152c7968c6c13fcd637a355bb253c3",
+    "url": "/projects/react-apps/tesla-battery/index.html"
+  }
+];
