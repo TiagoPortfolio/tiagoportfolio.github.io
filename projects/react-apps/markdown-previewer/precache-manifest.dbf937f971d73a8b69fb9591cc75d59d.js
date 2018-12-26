1a6429abc597036a602f6b0b15ef2a500fabb53f
TiagoPortfolio <tiagosousa028@gmail.com>
2018-12-26 18:41:19 +0000

    Update markdown previewer project

---
+++ projects/react-apps/markdown-previewer/precache-manifest.dbf937f971d73a8b69fb9591cc75d59d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projects/react-apps/markdown-previewer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c70cd5b6170e3c220cad",
+    "url": "/projects/react-apps/markdown-previewer/static/js/runtime~main.c70cd5b6.js"
+  },
+  {
+    "revision": "5f4e3bf6d592fbc3669c",
+    "url": "/projects/react-apps/markdown-previewer/static/js/main.5f4e3bf6.chunk.js"
+  },
+  {
+    "revision": "d5cbfadc9f6fa6434d9c",
+    "url": "/projects/react-apps/markdown-previewer/static/js/1.d5cbfadc.chunk.js"
+  },
+  {
+    "revision": "5f4e3bf6d592fbc3669c",
+    "url": "/projects/react-apps/markdown-previewer/static/css/main.08d642df.chunk.css"
+  },
+  {
+    "revision": "f1e57726df827d918d1fecc4ad0c7522",
+    "url": "/projects/react-apps/markdown-previewer/index.html"
+  }
+];
