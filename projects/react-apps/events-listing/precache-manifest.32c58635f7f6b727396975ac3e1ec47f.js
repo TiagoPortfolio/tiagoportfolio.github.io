c221b480a363fb2453d6b9c5778aaf1b6442c28d
TiagoPortfolio <tiagosousa28@hotmail.com>
2019-01-17 23:59:20 +0000

    add events listing react app

---
+++ projects/react-apps/events-listing/precache-manifest.32c58635f7f6b727396975ac3e1ec47f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "517d0242fa006c3c1dff",
+    "url": "/projects/react-apps/events-listing/static/js/runtime~main.517d0242.js"
+  },
+  {
+    "revision": "75c55ac35f94b69760fa",
+    "url": "/projects/react-apps/events-listing/static/js/main.75c55ac3.chunk.js"
+  },
+  {
+    "revision": "79180839fc9b3fb0d853",
+    "url": "/projects/react-apps/events-listing/static/js/1.79180839.chunk.js"
+  },
+  {
+    "revision": "75c55ac35f94b69760fa",
+    "url": "/projects/react-apps/events-listing/static/css/main.4a8a9db6.chunk.css"
+  },
+  {
+    "revision": "5059047f635189bb75a427af35ec8391",
+    "url": "/projects/react-apps/events-listing/index.html"
+  }
+];
