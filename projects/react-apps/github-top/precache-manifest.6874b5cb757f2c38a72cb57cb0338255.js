16ddb0d4f74e1e400159ff4b24ba1543d5db17ab
TiagoPortfolio <tiagosousa28@hotmail.com>
2019-01-28 21:55:36 +0000

    Add github-top project

---
+++ projects/react-apps/github-top/precache-manifest.6874b5cb757f2c38a72cb57cb0338255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffa7f36b9f8132950949eb842876ad8f",
+    "url": "/projects/react-apps/github-top/static/media/refresh-button.ffa7f36b.svg"
+  },
+  {
+    "revision": "7e2b4574daf94af0a2dc",
+    "url": "/projects/react-apps/github-top/static/js/runtime~main.7e2b4574.js"
+  },
+  {
+    "revision": "fe524b37d5fc2bc87030",
+    "url": "/projects/react-apps/github-top/static/js/main.fe524b37.chunk.js"
+  },
+  {
+    "revision": "7a7f5055f7b2704e702d",
+    "url": "/projects/react-apps/github-top/static/js/1.7a7f5055.chunk.js"
+  },
+  {
+    "revision": "fe524b37d5fc2bc87030",
+    "url": "/projects/react-apps/github-top/static/css/main.63357501.chunk.css"
+  },
+  {
+    "revision": "30fad050ec6b8a8bdc19f778a3981001",
+    "url": "/projects/react-apps/github-top/index.html"
+  }
+];
