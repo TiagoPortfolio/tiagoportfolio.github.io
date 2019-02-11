958e1e2860c2dbeaf796bc91754bd4b29f8c128e
TiagoPortfolio <tiagosousa028@gmail.com>
2019-02-11 19:25:23 +0000

    update events-listing app

--- projects/react-apps/events-listing/precache-manifest.9ce01225097f2fd986e35f29ad1aea8c.js
+++ projects/react-apps/events-listing/precache-manifest.9ce01225097f2fd986e35f29ad1aea8c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/projects/react-apps/events-listing/static/js/runtime~main.517d0242.js"
   },
   {
-    "revision": "36aa6dc3abf376055da7",
-    "url": "/projects/react-apps/events-listing/static/js/main.36aa6dc3.chunk.js"
+    "revision": "6aaeae64a98177081796",
+    "url": "/projects/react-apps/events-listing/static/js/main.6aaeae64.chunk.js"
   },
   {
     "revision": "b7f6af58d29c834cd98c",
     "url": "/projects/react-apps/events-listing/static/js/1.b7f6af58.chunk.js"
   },
   {
-    "revision": "36aa6dc3abf376055da7",
-    "url": "/projects/react-apps/events-listing/static/css/main.9750e1e6.chunk.css"
+    "revision": "6aaeae64a98177081796",
+    "url": "/projects/react-apps/events-listing/static/css/main.bcf0289d.chunk.css"
   },
   {
-    "revision": "57d0658dd23bb6001857c9d123dfcfa1",
+    "revision": "f88770bfea52afa1c5f23de9fd79cb9d",
     "url": "/projects/react-apps/events-listing/index.html"
   }
 ];
